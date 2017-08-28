d617e45f1c7b3792e696e0e0ec5098fe207a4c43
Dontavius03 <dontaviushopkins@ymail.com>
2017-08-27 20:55:33 -0400

    Comments

--- Comp280_Program-1/src/comp280_program/pkg1/Board.java
+++ Comp280_Program-1/src/comp280_program/pkg1/Board.java
@@ -15,6 +15,7 @@ import java.util.Scanner;
 public class Board {
     private int rows;
     private int cols;
+    // Created a 8x8 game board
     private char[][]game =  {
         {' ','1','2','3','4','5','6','7'},
         {'A','~','~','~','~','~','~','~'},
@@ -31,90 +32,91 @@ public class Board {
 
     }
     
+    // addShip method requires a ship type, location, and a direction in order to place ship on game board.
     public boolean addShip(String ship, String location, String direction){
 //        Scanner input = new Scanner(System.in);
 //        System.out.println("What type of ship? Submarine:2 units, Destroyer:3 units, Cruiser:4 units");
 //        ship = input.next().toLowerCase();
 //        System.out.println("What location will the ship be placed? Ex) A5");
 //        location = input.next().toUpperCase();
-        char x = location.charAt(0);
-        char y = location.charAt(1);
+        char x = location.charAt(0);//takes first character in the location string and assigns it to x
+        char y = location.charAt(1);//takes second character in the location string and assigns it to y
         int xCord = 0;
         int yCord = 0;
         
-        
+        //This switch statement allows me to assign an xCoordinate based on the first character of the location string variable
         switch(x){
-            case 'A':
+            case 'A':// if x is equal to 'A' then xCord == 1
             
                 xCord = 1;
             
             break;
-            case 'B':
+            case 'B':// if x is equal to 'B' then xCord == 2
             
                 xCord = 2;
             
             break;
-            case 'C':
+            case 'C':// if x is equal to 'C' then xCord == 3
             
                 xCord = 3;
             
             break;
-            case  'D':
+            case  'D':// if x is equal to 'D' then xCord == 4
             
                 xCord = 4;
             
             break;
-            case 'E':
+            case 'E':// if x is equal to 'E' then xCord == 5
             
                 xCord = 5;
             
             break;
-            case 'F':
+            case 'F':// if x is equal to 'F' then xCord == 6
             
                 xCord = 6;
             
             break;
-            case  'G':
+            case  'G':// if x is equal to 'G' then xCord == 7
     
                 xCord = 7;}
                 
-                
+          //This switch statement allows me to assign an yCoordinate based on the second character of the location string variable      
           switch(y){
-            case '1':
+            case '1':// if y is equal to '1' then yCord == 1
             
                 yCord = 1;
             
             break;
-            case '2':
+            case '2':// if y is equal to '2' then yCord == 2
             
                 yCord = 2;
             
             break;
-            case '3':
+            case '3':// if y is equal to '3' then yCord == 3
             
                 yCord = 3;
             
             break;
-            case  '4':
+            case  '4':// if y is equal to '4' then yCord == 4
             
                 yCord = 4;
             
             break;
-            case '5':
+            case '5':// if y is equal to '5' then yCord == 5
             
                 yCord = 5;
             
             break;
-            case '6':
+            case '6':// if y is equal to '6' then yCord == 6
             
                 yCord = 6;
             
             break;
-            case  '7':
+            case  '7':// if y is equal to '7' then yCord == 7
             
                 yCord = 7;}
 
-        
+        //This if statement takes user input and places '#' in the appropiate place
           if("submarine".equals(ship) && "horizontal".equals(direction) && game[xCord][yCord]== '~'){
               game[xCord][yCord]= '#';
               if(xCord==7){
@@ -243,7 +245,7 @@ public class Board {
     
     
     
-    
+    // this method just goes through the 2D array and prints each valaue.
     public void displayBoard(){
        
         for(int i=0;i<game.length;i++){
--- Comp280_Program-1/src/comp280_program/pkg1/Comp280_Program1.java
+++ Comp280_Program-1/src/comp280_program/pkg1/Comp280_Program1.java
@@ -8,6 +8,11 @@ package comp280_program.pkg1;
 /**
  *
  * @author Dontavius Hopkins
+ * Date:08/27/2017
+ * Assignment:Program#1-Battleship
+ * File: Comp280_Program1.java
+ * Description:I used this file to test my addShip and displayBoard methods
+ * 
  */
 public class Comp280_Program1 {
 
@@ -15,13 +20,13 @@ public class Comp280_Program1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // created a new Board object
         Board b = new Board();
    
-        
-        b.displayBoard();
-        b.addShip("submarine","E7", "horizontal");
-        b.displayBoard();
+        // Just adding diffrent ships and displaying them on the game board
+        b.displayBoard();// displays empty game 
+        b.addShip("submarine","E7", "horizontal");// places a 2 unit sub at location E7
+        b.displayBoard();// displays sub on the game board
         b.addShip("cruiser", "A5", "vertical");
         b.displayBoard();
         b.addShip("destroyer", "F1", "vertical");
